153e22be9e815b57dc0a6062a878eb9ea2aee6b5
Willian Prado <willian_prado@hotmail.com>
2022-04-04 22:56:03 -0300

    feat: add `most_frequent` function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,17 @@
+def most_frequent(arr):
+    frequencies = {}
+    most_frequent = arr[0]
+
+    for item in arr:
+        if item not in frequencies:
+            frequencies[item] = 1
+        else:
+            frequencies[item] += 1
+        if frequencies[item] > frequencies[most_frequent]:
+            most_frequent = item
+
+    return most_frequent
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
